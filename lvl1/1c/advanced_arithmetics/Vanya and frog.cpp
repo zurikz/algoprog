7cce25229b5693c1b74522ebca679e7fcebb17e1
Zurab <zurabnurmag@gmail.com>
2021-05-31 22:28:29 +0300

    Vanya (unfinished)

---
+++ lvl1/1c/advanced_arithmetics/Vanya and frog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int solution(const int& bound, const int& seconds) {
+    
+    return coordinate;
+}
+
+int main() {
+
+    return 0;
+}
