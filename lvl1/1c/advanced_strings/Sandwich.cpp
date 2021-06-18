b32d2e195a4bf16615bd41d28b25a7704de923c9
Zurab <zurabnurmag@gmail.com>
2021-06-18 09:52:43 +0300

    Update Sandwich

--- lvl1/1c/advanced_strings/Sandwich.cpp
+++ lvl1/1c/advanced_strings/Sandwich.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 string decrypt(const string& encrypted) {
     int len = encrypted.size() - 1;
-    char decrypted[len];
+    string decrypted(len, ' ');
     for (int i = 0; i < len; i++) {
         if (i % 2 == 0) {
             decrypted[i / 2] = encrypted[i];
@@ -14,40 +14,10 @@ string decrypt(const string& encrypted) {
             decrypted[(len - 1) - (i / 2)] = encrypted[i];
         }
     }
-    return string(decrypted);
-}
-
-void readTest(ifstream& file, string& encrypted, string& answer) {
-    if (file.is_open()) {
-        string line;
-        getline(file, line);
-        getline(file, encrypted);
-        getline(file, line);
-        getline(file, answer);
-        getline(file, line);
-    }
-}
-
-int test(ifstream& file) {
-    while (file) {
-        string encrypted, answer;
-        readTest(file, encrypted, answer);
-        string decrypted = decrypt(encrypted);
-        if (decrypted != answer) {
-            cout << "Failed test:" << endl;
-            cout << encrypted << endl;
-            cout << "Answer:    " << answer << endl;
-            cout << "My answer: " << decrypted << endl;
-            return 1;
-        }
-    }
-    cout << "OK!";
-    return 0;
+    return decrypted;
 }
 
 int main() {
-    // ifstream file("Sandwich.txt");
-    // test(file);
     string input;
     cin >> input;
     cout << decrypt(input);
