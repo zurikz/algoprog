7dffe6729d67b0462afa0dcaa51770301e612508
Zurab <zurabnurmag@gmail.com>
2021-06-22 12:06:34 +0300

    Update Clock - 2.cpp

--- lvl1/1c/advanced_real/Clock - 2.cpp
+++ lvl1/1c/advanced_real/Clock - 2.cpp
@@ -5,38 +5,11 @@
 
 using namespace std;
 
+const int oneHourAngle = 360 / 12;
+
 double solve(double alpha) {
-    return 12.0 * (alpha - (30 * (int(alpha) / 30)));
-}
-
-void readTest(ifstream& file, double& input, double& answer) {
-    if (file.is_open()) {
-        string line;
-        getline(file, line);
-        file >> input;
-        file.ignore();
-        getline(file, line);
-        file >> answer;
-        file.ignore();
-        getline(file, line);
-    }
-}
-
-int test(ifstream& file) {
-    while (file) {
-        double input, answer;
-        readTest(file, input, answer);
-        double solution = solve(input);
-        if (solution != answer) {
-            cout << "Failed test:" << endl;
-            cout << "Input: " << input << endl;
-            cout << "Answer: " << answer << endl;
-            cout << "My answer: " << solution;
-            return 1;
-        }
-    }
-    cout << "OK!";
-    return 0;
+    double moduloHourAngle = alpha - oneHourAngle * (int(alpha) / oneHourAngle);
+    return 12 * moduloHourAngle;
 }
 
 int main() {
