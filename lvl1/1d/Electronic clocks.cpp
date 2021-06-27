209d1c36ecd42a910cd881b9e5d21f4c47ffd1f3
Zurab <zurabnurmag@gmail.com>
2021-06-27 13:41:58 +0300

    Update Electronic clocks.cpp

--- lvl1/1d/Electronic clocks.cpp
+++ lvl1/1d/Electronic clocks.cpp
@@ -1,51 +1,19 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 
 using namespace std;
 
-string solve(int seconds) {
-    string answer;
+void showClocks(int seconds) {
     int hrs = seconds / (60 * 60);
-    answer += to_string(hrs % 24) + ':';
+    cout << hrs % 24 << ':';
     int mins = (seconds - hrs * 60 * 60) / 60;
-    answer += to_string(mins / 10) + to_string(mins % 10) + ':';
+    cout << mins / 10 << mins % 10 << ':';
     int secs = seconds - hrs * 60 * 60 - mins * 60;
-    answer += to_string(secs / 10) + to_string(secs % 10);
-    return answer;
-}
-
-void readTest(ifstream& file, int& seconds, string& answer) {
-    if (file.is_open()) {
-        string line;
-        getline(file, line);
-        file >> seconds;
-        file.ignore();
-        getline(file, line);
-        getline(file, answer);
-        getline(file, line);
-    }
-}
-
-int test(ifstream& file) {
-    while (file) {
-        int seconds;
-        string answer;
-        readTest(file, seconds, answer);
-        string solution = solve(seconds);
-        if (solution != answer) {
-            cout << "Input: " << seconds << endl;
-            cout << "Answer: " << answer << endl;
-            cout << "My answer: " << solution;
-            return 1;
-        }
-    }
-    cout << "OK!";
-    return 0;
+    cout << secs / 10 << secs % 10;
 }
 
 int main() {
-    ifstream file("Electronic clocks.txt");
-    test(file);
+    int seconds;
+    cin >> seconds;
+    showClocks(seconds);
     return 0;
 }
