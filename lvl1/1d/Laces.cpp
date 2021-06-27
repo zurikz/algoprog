75ffbe4d1c6b7534dc16898af2ee7705cb1d44b3
Zurab <zurabnurmag@gmail.com>
2021-06-27 15:26:19 +0300

    Update Laces.cpp

--- lvl1/1d/Laces.cpp
+++ lvl1/1d/Laces.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 
 using namespace std;
 
@@ -8,38 +6,9 @@ int solve(int a, int b, int l, int N) {
     return a + (N - 1) * 2 * a + (N - 1) * 2 * b + 2 * l;
 }
 
-void readTest(ifstream& file, int& a, int& b, int& l, int& N, int& answer) {
-    if (file.is_open()) {
-        string line;
-        getline(file, line);
-        file >> a >> b >> l >> N;
-        file.ignore();
-        getline(file, line);
-        file >> answer;
-        file.ignore();
-        getline(file, line);
-    }
-}
-
-int test(ifstream& file) {
-    while (file) {
-        int a, b, l, N, answer;
-        readTest(file, a, b, l, N, answer);
-        int solution = solve(a, b, l, N);
-        if (solution != answer) {
-            cout << "Input: " << a << " " 
-            << b << " " << l << " " << N << endl;
-            cout << "Answer: " << answer << endl;
-            cout << "My answer: " << solution;
-            return 1;
-        }
-    }
-    cout << "OK!";
-    return 0;
-}
-
 int main() {
-    ifstream file("Laces.txt");
-    test(file);
+    int a, b, l, N;
+    cin >> a >> b >> l >> N;
+    cout << solve(a, b, l, N);
     return 0;
 }
