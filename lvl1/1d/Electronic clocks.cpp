783d2c21b87617cd56d32367710d69e8f1f60363
Zurab <zurabnurmag@gmail.com>
2021-06-27 15:26:12 +0300

    Update Electronic clocks.cpp

--- lvl1/1d/Electronic clocks.cpp
+++ lvl1/1d/Electronic clocks.cpp
@@ -5,9 +5,9 @@ using namespace std;
 void showClocks(int seconds) {
     int hrs = seconds / (60 * 60);
     cout << hrs % 24 << ':';
-    int mins = (seconds - hrs * 60 * 60) / 60;
+    int mins = (seconds % (60 * 60)) / 60;
     cout << mins / 10 << mins % 10 << ':';
-    int secs = seconds - hrs * 60 * 60 - mins * 60;
+    int secs = seconds % 60;
     cout << secs / 10 << secs % 10;
 }
 
