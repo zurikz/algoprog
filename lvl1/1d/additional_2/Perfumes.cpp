5da61d14165d6f2b110002c5ed277c57a74fb87a
Zurab <zurabnurmag@gmail.com>
2021-07-22 21:09:01 +0300

    Create Perfumes.cpp

---
+++ lvl1/1d/additional_2/Perfumes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int flasks, types;
+    cin >> flasks >> types;
+    double flasksData[flasks + 1][types + 1];
+    for (int i = 1; i <= flasks; i++) {
+        int type;
+        double volume;
+        cin >> type >> volume;
+        flasksData[i][type] = volume;
+    }
+
+    int k;
+    cin >> k; 
+    for (int i = 0; i < k; i++) {
+        int originFlask, destinationFlask, percent;
+        cin >> originFlask >> destinationFlask >> percent;
+        for (int type = 1; type <= types; type++) {
+            double volume = flasksData[originFlask][type] * (percent / 100.0);
+            flasksData[destinationFlask][type] += volume;
+            flasksData[originFlask][type] -= volume;
+        }
+    }
+
+    double sum = 0;
+    for (int type = 1; type <= types; type++) {
+        sum += flasksData[1][type];
+    }
+
+    for (int type = 1; type <= types; type++) {
+        cout << fixed << setprecision(3) << 100 * flasksData[1][type] / sum << " ";
+    }
+    return 0;
+}
