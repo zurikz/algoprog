8116f23c643fb9469c5c3622965faf139bce757a
Zurab <zurabnurmag@gmail.com>
2021-08-15 11:12:21 +0300

    Update Hanoi.cpp

--- lvl2/2a/recursion/Hanoi.cpp
+++ lvl2/2a/recursion/Hanoi.cpp
@@ -5,24 +5,18 @@
 using namespace std;
 
 void solveTowerOfHanoi(
-    vector<stack<unsigned>>& rods, int n, int source, int target, int medium
+    int n, int source, int target, int medium
 ) {
     if (n > 0) {
-        solveTowerOfHanoi(rods, n - 1, source, medium, target);
-        rods[target].push(rods[source].top());
-        rods[source].pop();
+        solveTowerOfHanoi(n - 1, source, medium, target);
         cout << n << " " << source + 1 << " " << target + 1 << endl;
-        solveTowerOfHanoi(rods, n - 1, medium, target, source);
+        solveTowerOfHanoi(n - 1, medium, target, source);
     }
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<stack<unsigned>> rods(3);
-    for (unsigned i = n; i > 0; --i) {
-        rods[0].push(i);
-    }
-    solveTowerOfHanoi(rods, n, 0, 2, 1);
+    solveTowerOfHanoi(n, 0, 2, 1);
     return 0;
 }
