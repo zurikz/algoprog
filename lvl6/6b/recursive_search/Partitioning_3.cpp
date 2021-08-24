366f730178b22f687698a3171456af7c617a3a47
Zurab <zurabnurmag@gmail.com>
2021-08-24 12:35:32 +0300

    Partitioning_3.cpp

--- lvl6/6b/recursive_search/Partitioning_3.cpp
+++ lvl6/6b/recursive_search/Partitioning_3.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 
 void check(const vector<int>& a) {
-    for (const int& x : a) {
-        cout << x << " ";
+    for (size_t i = 1; i < a.size(); i++) {
+        cout << a[i] << " ";
     }
     cout << '\n';
 }
 
-void partition(vector<int>& a, int n, int sum) {
-    if (sum == n) {
+void partition(vector<int>& a, int n, int i) {
+    if (n == 0) {
         check(a);
         return;
     }
-    for (int j = 1; j <= n; j++) {
-        a.push_back(j);
-        sum += j;
-        partition(a, n, sum);
-        sum -= j;
-        a.pop_back();
+    for (int j = a[i - 1]; j <= n; j++) {
+        if (n - j >= 0) {
+            a.push_back(j);
+            partition(a, n - j, i + 1);
+            a.pop_back();
+        }
     }
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> a;
-    partition(a, n, 0);
+    vector<int> a = {1};
+    partition(a, n, 1);
     return 0;
 }
