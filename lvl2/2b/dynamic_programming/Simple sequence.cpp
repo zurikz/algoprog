882df9bee8609fd29b638c47358a867a09e900fc
Zurab Nurmagomedov <zurabnurmag@gmail.com>
2021-09-08 15:02:06 +0300

    Update Simpe sequence.cpp

--- lvl2/2b/dynamic_programming/Simple sequence.cpp
+++ lvl2/2b/dynamic_programming/Simple sequence.cpp
@@ -8,8 +8,11 @@ int seq(int n) {
     vector<int> ans(n + 1);
     ans[0] = ans[1] = 1;
     for (size_t i = 2; i < ans.size(); i++) {
-        bool isEven = (i % 2 == 0);
-        ans[i] = ans[i / 2] + isEven * ans[(i / 2) - 1] - (!isEven) * ans[(i / 2) - 1];
+        if (i % 2 == 0) {
+            ans[i] = ans[i / 2] + ans[(i / 2) - 1];
+        } else {
+            ans[i] = ans[i / 2] - ans[(i / 2) - 1];
+        }
     }
     return ans[n];
 }
