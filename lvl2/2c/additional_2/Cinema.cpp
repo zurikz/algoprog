1ebc9847fe0c919ea68698dc481ee407ac0e27a3
Zurab <zurabnurmag@gmail.com>
2021-10-06 15:34:30 +0300

    Cinema.cpp

--- lvl2/2c/additional_2/Cinema.cpp
+++ lvl2/2c/additional_2/Cinema.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
-
-pair<int, int> newCoord(pair<int, int> coord, int n, int m) {
-    int num = coord.first * m + coord.second;
-    return {num % n, num / n};
-}
-
-int solve(int n, int m) {
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (pair<int, int> {i, j} == newCoord({i, j}, n, m)) {
-                ++ans;
-            }
-        }
-    }
-    return ans;
-}
 
 int gcd(int a, int b) {
     while (b != 0) {
@@ -29,19 +11,17 @@ int gcd(int a, int b) {
     return a;
 }
 
-// int solve2(int n, int m) {
-
-// }
+int solve(int n, int m) {
+    if ((n == 1) || (m == 1)) {
+        return max(n, m);
+    } else {
+        return 1 + gcd(n - 1, m - 1);
+    }
+}
 
 int main() {
     int n, m;
     cin >> n >> m;
-    cout << solve(n, m) << endl;
-    cout << gcd(n - 1, m - 1); 
-    
-    // int num;
-    // cin >> num;
-    // pair<int, int> newCoo = newCoord(num, n, m);
-    // cout << newCoo.first << " " << newCoo.second;
+    cout << solve(n, m);
     return 0;
 }
